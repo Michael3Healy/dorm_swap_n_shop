233e2bab9a8fa41f549a5b078ac0876fec02445a
Michael Healy <michael.healy30@yahoo.com>
2024-08-09 16:32:17 -0400

    Frontend and backend created, db seeded

---
+++ backend/config.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** Shared config for application; can be required many places. */
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+
+const PORT = +process.env.PORT || 3001;
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+      ? `postgresql://${USERNAME}:${PASSWORD}@localhost/dorm_shop_test`
+      : process.env.DATABASE_URL || `postgresql://${USERNAME}:${PASSWORD}@localhost/dorm_shop`;
+}
+
+let database = process.env.NODE_ENV === 'test' ? 'dorm_shop' : 'dorm_shop_test';
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("dorm_shop Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, database);
+console.log("---");
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+};
